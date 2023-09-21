d55901b73b25278c3cd4ca283c14fb45d46323ed
alfredo calero <acfvgo89@gmail.com>
2023-09-21 01:16:18 -0500

    correciones del mapeo de usuario y roles

--- Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/Usuario.java
+++ Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/Usuario.java
@@ -23,4 +23,87 @@ public class Usuario {
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
     private Set<UsuarioRol> usuarioRoles = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public Set<UsuarioRol> getUsuarioRoles() {
+        return usuarioRoles;
+    }
+
+    public void setUsuarioRoles(Set<UsuarioRol> usuarioRoles) {
+        this.usuarioRoles = usuarioRoles;
+    }
+
+    public Usuario() {
+    }
 }
--- Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/UsuarioRol.java
+++ Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/UsuarioRol.java
@@ -9,9 +9,36 @@ public class UsuarioRol {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long usuarioRolId;
 
-    @ManyToMany(fetch =  FetchType.EAGER)
+    @ManyToOne(fetch =  FetchType.EAGER)
     private Usuario usuario;
 
-    @ManyToMany()
+    @ManyToOne()
     private Rol rol;
+
+    public Long getUsuarioRolId() {
+        return usuarioRolId;
+    }
+
+    public void setUsuarioRolId(Long usuarioRolId) {
+        this.usuarioRolId = usuarioRolId;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public UsuarioRol() {
+    }
 }
