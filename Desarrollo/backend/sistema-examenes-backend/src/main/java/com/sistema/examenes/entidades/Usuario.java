68bdb4c99b1fd1ae32f4dc33a526e056b7d340ce
alfredo calero <acfvgo89@gmail.com>
2023-09-21 00:55:18 -0500

    Clase Usuario

--- Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/Usuario.java
+++ Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/Usuario.java
@@ -1,2 +1,26 @@
-package com.sistema.examenes.entidades;public class Usuario {
+package com.sistema.examenes.entidades;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoincrementable
+    private Long id;
+    private String username;
+    private String password;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String telefono;
+    private boolean enable = true;
+    private String perfil;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "usuario")
+    private Set<UsuarioRol> usuarioRoles = new HashSet<>();
 }
