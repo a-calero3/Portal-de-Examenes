d85d1246fdee06fb20de3ade59c9dc3ffe016594
alfredo calero <acfvgo89@gmail.com>
2023-09-21 00:55:39 -0500

    Clase Rol Usuario

--- Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/UsuarioRol.java
+++ Desarrollo/backend/sistema-examenes-backend/src/main/java/com/sistema/examenes/entidades/UsuarioRol.java
@@ -1,2 +1,17 @@
-package com.sistema.examenes.entidades;public class UsuarioRol {
+package com.sistema.examenes.entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UsuarioRol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long usuarioRolId;
+
+    @ManyToMany(fetch =  FetchType.EAGER)
+    private Usuario usuario;
+
+    @ManyToMany()
+    private Rol rol;
 }
